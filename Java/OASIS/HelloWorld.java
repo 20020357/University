5e43e8e0412b52bf496fcabfc6c5293d77836371
bta0357uetvnu <20020357>
2021-10-30 06:43:45 +0700

    add HelloWorld file

---
+++ Java/OASIS/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+HelloWorld.
+ */
+public class HelloWorld {
+  public static String printHello() {
+    return "HelloWorld";
+  }
+
+  public static void main(String[] args) {
+    System.out.println(printHello());
+  }
+}
