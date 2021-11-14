f11c456fd39e0ba38816966865fe1bca3d81d27a
bta0357uetvnu <20020357>
2021-11-14 20:45:28 +0700

    update Chess folder and add Generic folder

---
+++ Java/OASIS/Chess/src/Move.java
@@ -0,0 +1,10 @@
+public class Move {
+  private int startX;
+  private int endX;
+  private int startY;
+  private int endY;
+  private Piece movedPiece;
+  private Piece killedPiece;
+
+  
+}
--- Java/OASIS/Chess/src/Piece.java
+++ Java/OASIS/Chess/src/Piece.java
@@ -7,6 +7,7 @@ public abstract class Piece {
   public Piece(int coordinatesX, int coordinatesY) {
     this.coordinatesX = coordinatesX;
     this.coordinatesY = coordinatesY;
+    color = "white";
   }
 
   /**Initialize Piece with coordinates X, Y and color. */
@@ -16,8 +17,37 @@ public abstract class Piece {
     this.color = color;
   }
 
+  /**Getter coordinatesX. */
+  public int getCoordinatesX() {
+    return coordinatesX;
+  }
+
+  /**Setter coordinatesX. */
+  public void setCoordinatesX(int coordinatesX) {
+    this.coordinatesX = coordinatesX;
+  }
+
+  /**Getter coordinatesY. */
+  public int getCoordinatesY() {
+    return coordinatesY;
+  }
+
+  /**Setter coordinatesY. */
+  public void setCoordinatesY(int coordinatesY) {
+    this.coordinatesY = coordinatesY;
+  }
+
+  /**Getter color. */
+  public String getColor() {
+    return color;
+  }
+
+  /**Setter color. */
+  public void setColor(String color) {
+    this.color = color;
+  }
+
   /**Getter Symbol. */
   public abstract String getSymbol();
-  public abstract boolean canMove();
-
+  public abstract boolean canMove(Board board, int x, int y);
 }
